b0fd7fceed096cfe9bd7bd78a2fbef423c3297da
eliasroman2565 <elias.roman2565@gmail.com>
2017-01-26 14:13:10 -0500

    1/26 Final 1.1

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 // Enemies our player must avoid
 var Enemy = function(x, y, speed) {
-  this.x = x;
-  this.y = y;
-  this.speed = speed;
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -10,20 +10,18 @@ var Enemy = function(x, y, speed) {
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
-var charWidth = 171;
-var charHeight = 101;
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-  this.x = this.x + 101 * dt;
-  this.x = this.x + this.speed;
+    this.x = this.x + 101 * dt;
+    this.x = this.x + this.speed;
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
     if (this.x > 505) {
-      this.x = 0;
-    };
+        this.x = 0;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -32,51 +30,52 @@ Enemy.prototype.render = function() {
 };
 // Define Player Class
 var Player = function(x, y, speed) {
-  this.x = x;
-  this.y = y;
-  this.speed = speed;
-  this.sprite = 'images/char-boy.png'
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.sprite = 'images/char-boy.png';
 };
 // Rest Function back to starting position
 Player.prototype.reset = function() {
-  this.x = 303;
-  this.y = 303;
-}
+    this.x = 303;
+    this.y = 303;
+};
 
 Player.prototype.update = function(dt) {
-  this.x = this.x;
-  this.y = this.y;
-// Check if player goes off map
-  if ( this.x > 505){
-    player.reset();
-  };
-  if (this.x < 0) {
-    player.reset();
-  };
-  if (this.y > 450) {
-    player.reset();
-  };
-  if (this.y < 0) {
-    player.reset();
-    alert("You Win, Bitch");
-  };
-  // Check for Collisions
+    this.x = this.x;
+    this.y = this.y;
+    // Check if player goes off map
+    if (this.x > 505) {
+        player.reset();
+    }
+    if (this.x < 0) {
+        player.reset();
+    }
+    if (this.y > 450) {
+        player.reset();
+    }
+    if (this.y < 0) {
+        player.reset();
+        alert("You Win, Bitch");
+    }
 
-allEnemies.forEach(function(Enemy){
-    if (player.x < Enemy.x + 50 &&
-    player.x  > Enemy.x  &&
-    player.y < Enemy.y + 50 &&
-    player.y  > Enemy.y) {
-      player.reset();
-      console.log("collision");
-    };
+    // Check for Collisions
+
+    allEnemies.forEach(function(Enemy) {
+        if (player.x < Enemy.x + 50 &&
+            player.x > Enemy.x &&
+            player.y < Enemy.y + 50 &&
+            player.y > Enemy.y) {
+            player.reset();
+            console.log("collision");
+        }
 
     });
 
 };
 // Draw Player on Canvas
 Player.prototype.render = function() {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 //Function for Player Controls
 Player.prototype.handleInput = function(keyPress) {
